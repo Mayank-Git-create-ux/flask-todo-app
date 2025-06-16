f01582003959466cd81381d9afded8935319bdac
Mayank-Git-create-ux <mayank.patel54321@gmail.com>
2025-06-16 16:59:07 +0530

    Create models.py

---
+++ app/models.py
@@ -0,0 +1,7 @@
+from app import db
+
+# Todo model
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(200), nullable=False)
+    done = db.Column(db.Boolean, default=False)
